2bdc42ad79fe2c302cdac297ba3c175116d14a74
Dmitriy-Evs <dmitevseev@gmail.com>
2024-12-08 18:07:17 +0500

    Update noEvery.js

--- noEvery.js
+++ noEvery.js
@@ -11,9 +11,9 @@ let pointX;
 
 //Считаем количество точек в зоне для заполнения
 let countPoints = row * column - (row * 2 + column * 2 - 4);
-let randomPointsArr = [[],[]]; // Создаем массив, который содержит вложенные массивы, в котором хранятся случайные точки
+let randomPointsArr = [[]]; // Создаем массив, который содержит вложенные массивы, в котором хранятся случайные точки
 let createdOneDot = []; // Это массив, содержащий одну точку, который будет добавлен в основной.
-//console.log("Количество точек " + countPoints);
+console.log("Количество точек " + countPoints);
 
 //Создаем случайную точку, исключаем значения на границах
 function createArrayOfDot(row, column) {
@@ -23,36 +23,41 @@ function createArrayOfDot(row, column) {
 }
 
 //Создаем первую точку
-createArrayOfDot(row, column);
+//createArrayOfDot(row, column);
 //Создаем первую точку в массиве randomPointsArr
 
 
-/* randomPointsArr[0][0] = createdOneDot[0]
-randomPointsArr[0][1] = createdOneDot[1] */
-console.log("Весь массив перед циклом " + randomPointsArr)
+randomPointsArr = createdOneDot
+
+//console.log("Весь массив перед циклом " + randomPointsArr)
 //console.log(createdOneDot)
 
 
-for (let p = 0; p <= countPoints;) {
-    console.log("Точка в начале цикла " + createdOneDot)
-    
 
+for (p = 1; p <= countPoints; ) {
+    createArrayOfDot(row, column)
     //for (let j = 0; j <= createdOneDot.length - 1; j++) {
-        if (createdOneDot[0] !== randomPointsArr[p][0] && createdOneDot[1] !== randomPointsArr[p][1]) {
-            randomPointsArr.push(createdOneDot);
-            p++; //Увеличиваем шаг только тогда, когда точка создана
-        } else {
-            createArrayOfDot(row, column); //Иначе создаем новую
-            //console.log(createdOneDot);
-        }
-    //}
+    if (exist(randomPointsArr, createdOneDot)) {
+        createArrayOfDot(row, column)
+    } else {
+        randomPointsArr.push(createdOneDot);
+        p++; 
+    }
 }
 
-// Проверяем, что каждый элемент массива randomPointsArr не равен createdOneDot
-    //Условие проверки я спросил у гпт
+function exist(randomPointsArr, createdOneDot) {
+    for (let i = 1; i <= randomPointsArr.length - 1; i++) {
+        return randomPointsArr[i] === createdOneDot
+    }
+}
 
-    /* randomPointsArr.every((value) =>
-      value.some((val, index) => val !== createdOneDot[index]) // Переписать без every
-    ) */
+
+
+// Проверяем, что каждый элемент массива randomPointsArr не равен createdOneDot
+//Условие проверки я спросил у гпт
+
+/* randomPointsArr.every((value) =>
+  value.some((val, index) => val !== createdOneDot[index]) // Переписать без every
+) */
 console.log(randomPointsArr);
 console.log("Длина главного массива " + randomPointsArr.length);
